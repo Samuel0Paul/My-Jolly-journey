0c57b5902cd33d85556dcc8bd634413b8ad5f925
Samuel0Paul <paulsamuelvishesh@live.com>
2017-03-01 11:35:19 +0530

    lib support for loading ALPHA channel into textures

--- lib/mylib.hpp
+++ lib/mylib.hpp
@@ -206,6 +206,31 @@ namespace mylib
 		return texID;
 	}
 
+	static GLuint loadTextureRGB(GLchar* path) {
+		return loadTexture(path);
+	}
+
+	static GLuint loadTextureRGBA(GLchar* path)
+	{
+		GLuint texID{ 0 };
+		glGenTextures(1, &texID);
+		int width{ 0 }, height{ 0 };
+		unsigned char* image = SOIL_load_image(path, &width, &height,
+			0, SOIL_LOAD_RGBA);
+		glBindTexture(GL_TEXTURE_2D, texID);
+		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height,
+			0, GL_RGBA, GL_UNSIGNED_BYTE, image);
+		glGenerateMipmap(GL_TEXTURE_2D);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+		glBindTexture(GL_TEXTURE_2D, 0);
+		SOIL_free_image_data(image);
+
+		return texID;
+	}
+
 };
 
 #endif
